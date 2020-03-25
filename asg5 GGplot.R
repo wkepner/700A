2ebbe26ab9974b139328f6d6c458761231d08b16
wkepner <wkepner@gmail.com>
2020-03-24 22:21:52 -0700

    Update asg5 GGplot.R

--- asg5 GGplot.R
+++ asg5 GGplot.R
@@ -30,6 +30,24 @@ ggplot(birthwt, aes(factor(race),bwt))+geom_boxplot()+facet_grid(race~.)
 ggplot(iris,aes(Sepal.Length,Sepal.Width,color=Species)) +geom_point(size=2) + facet_grid(Species~.)
 ggplot(iris,aes(Sepal.Length,Sepal.Width,color=Species)) +geom_point(size=2) + facet_grid(.~Species)
 
+
+
+library(ggplot2)
+ggplot(data = FiveYearData, aes(x = year, y = lifeExp, color=continent)) +
+  geom_line() +
+  facet_wrap( ~ country) +
+  ggtitle("Figure 1") +
+  xlab("Year")+
+  ylab("Life Expectancy")+
+  scale_colour_discrete("Continent")
+
+data(mtcars)
+attach(mtcars)
+detach(cars)
+
+ggplot(mtcars, aes(wt,mpg, color=cyl))+geom_point(aes(shape=factor(cyl), size=2))
+
+
 #Exercise 21
 
 library(ggplot2)
@@ -47,5 +65,3 @@ data(diamonds)
 myplot<-ggplot(diamonds,aes(carat,price,color=clarity))+
   geom_point(alpha=0.1)+geom_smooth(se=F)+
   theme_minimal()
-  
-
